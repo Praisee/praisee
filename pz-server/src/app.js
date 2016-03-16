85e077360c37e43d27bc9517b39784e7533dbf76
James Koshigoe <james@jameskoshigoe.com>
2016-03-15 20:58:31 -0600

    Restructured directory paths to be more node-esque

--- pz-server/src/app.js
+++ pz-server/src/app.js
@@ -5,11 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', require.resolve('pz-server/src/views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -18,10 +17,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(require.resolve('pz-client/build/public')));
 
-var productDataSource = require('./product/inMemoryDataSource')();
-var productRoutes = require('./product/productRoutes')(productDataSource);
+var productDataSource = require('pz-server/src/product/in-memory-data-source')();
+var productRoutes = require('pz-server/src/product/product-routes')(productDataSource);
 app.use('/api/v1/product', productRoutes);
 
 // catch 404 and forward to error handler
