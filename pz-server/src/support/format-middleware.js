7ccd4af51d51a90b15f82d253eaa01d27768ee1f
James Koshigoe <james@jameskoshigoe.com>
2016-04-10 14:55:54 -0600

    Cleaning out current server files

--- pz-server/src/support/format-middleware.js
+++
@@ -1,8 +0,0 @@
-// Example taken from:
-// https://github.com/expressjs/express/blob/master/examples/content-negotiation/index.js
-
-export default function formatMiddleware(handler) {
-    return (request, response) => {
-        return response.format(handler(request, response));
-    }
-};
