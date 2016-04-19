80d63c80ec96015265357b3e663c529baad94414
Luke Pammant <luke.pammant@gmail.com>
2016-04-18 23:31:58 -0600

    Add reputation aggregation via SQL query

--- pz-domain/src/models/reviewer.ts
+++ pz-domain/src/models/reviewer.ts
@@ -1,9 +1,24 @@
-exports = function(Reviewer) {
-    Reviewer.reputation = function(reviewerId, cb) {
-        Reviewer.findById(reviewerId, function(err, reviewer) {
-            reviewer.reviews((err, reviews) => {
-                reviews.forEach((r) => console.log(r));
-                cb(null, reviews);
+module.exports = function (Reviewer) {
+    Reviewer.reputation = function (reviewerId, cb) {
+        Reviewer.findById(reviewerId, function (err, reviewer) {
+            if (err) return console.log(err);
+            if (!reviewer) return cb("Not found");
+
+            var ds = Reviewer.dataSource;
+            var votesQuery = `SELECT Vote.upVote FROM Reviewer
+                            JOIN Review ON Reviewer.Id = Review.ReviewerId
+                            JOIN Vote ON Review.Id = Vote.ReviewerId
+                            WHERE Reviewer.id = ${reviewerId}`;
+
+            ds.connector.execute(votesQuery, function (err, result) {
+                if (err) console.error(err);
+
+                var reputation = 0;
+                for (var i = result.length; i--;) {
+                    reputation += result[i] ? 1 : -1;
+                }
+                console.info(reputation);
+                cb(err, reputation);
             });
         });
     };
