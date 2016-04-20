349bf90304cfd8fdb07b38db0cf8c8efc9834833
Luke Pammant <luke.pammant@gmail.com>
2016-04-19 22:27:46 -0600

    Made reviewer.ts more ES6 ish

--- pz-domain/src/models/reviewer.ts
+++ pz-domain/src/models/reviewer.ts
@@ -1,23 +1,23 @@
 module.exports = function (Reviewer) {
     Reviewer.reputation = function (reviewerId, cb) {
         Reviewer.findById(reviewerId, function (err, reviewer) {
-            if (err) return console.log(err);
-            if (!reviewer) return cb("Not found");
+            if (err) { return console.log(err); }
+            if (!reviewer) { return cb("Not found"); }
 
-            var ds = Reviewer.dataSource;
-            var votesQuery = `SELECT Vote.upVote FROM Reviewer
-                            JOIN Review ON Reviewer.Id = Review.ReviewerId
-                            JOIN Vote ON Review.Id = Vote.ReviewerId
-                            WHERE Reviewer.id = ${reviewerId}`;
+            const ds = Reviewer.dataSource;
+            const votesQuery = `SELECT Vote.upVote FROM Reviewer
+                                JOIN Review ON Reviewer.Id = Review.ReviewerId
+                                JOIN Vote ON Review.Id = Vote.ReviewerId
+                                WHERE Reviewer.id = ${reviewerId}`;
 
-            ds.connector.execute(votesQuery, function (err, result) {
-                if (err) console.error(err);
+            ds.connector.execute(votesQuery, (err, result) => {
+                if (err) { return console.error(err); }
 
-                var reputation = 0;
-                for (var i = result.length; i--;) {
-                    reputation += result[i] ? 1 : -1;
+                let reputation = 0;
+                for (let vote of result) {
+                    reputation += vote ? 1 : -1;
                 }
-                console.info(reputation);
+
                 cb(err, reputation);
             });
         });
