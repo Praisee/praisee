3e91e4cd982c204ba281b45255e794295dae13dc
James Koshigoe <james@jameskoshigoe.com>
2016-05-09 15:58:26 -0600

    New domain models

--- pz-server/src/model-config.ts
+++ pz-server/src/model-config.ts
@@ -1,60 +1,74 @@
 import pzPath from 'pz-support/src/pz-path';
 
 export default {
-  '_meta': {
-    'sources': [
-      'loopback/common/models',
-      'loopback/server/models',
-      pzPath('pz-domain', 'src/models'),
-      './models'
-    ],
-    'mixins': [
-      'loopback/common/mixins',
-      'loopback/server/mixins',
-        pzPath('pz-domain', 'src/mixins'),
-      './mixins'
-    ]
-  },
-  'User': {
-    'dataSource': 'memory-db',
-    'public': false
-  },
-  'AccessToken': {
-    'dataSource': 'memory-db',
-    'public': false
-  },
-  'ACL': {
-    'dataSource': 'memory-db',
-    'public': false
-  },
-  'RoleMapping': {
-    'dataSource': 'memory-db',
-    'public': false
-  },
-  'Role': {
-    'dataSource': 'memory-db',
-    'public': false
-  },
-  'Product': {
-    'dataSource': 'vagrant-postgres',
-    'public': true,
-    '$promise': {},
-    '$resolved': true
-  },
-  'Review': {
-    'dataSource': 'vagrant-postgres',
-    'public': true,
-    '$promise': {},
-    '$resolved': true
-  },
-  'Reviewer': {
-    'dataSource': 'vagrant-postgres',
-    'public': true,
-    '$promise': {},
-    '$resolved': true
-  },
-  'Vote': {
-    'dataSource': 'vagrant-postgres',
-    'public': true
-  }
+    '_meta': {
+        'sources': [
+            'loopback/common/models',
+            'loopback/server/models',
+            pzPath('pz-domain', 'src/models'),
+            './models'
+        ],
+        'mixins': [
+            'loopback/common/mixins',
+            'loopback/server/mixins',
+            pzPath('pz-domain', 'src/mixins'),
+            './mixins'
+        ]
+    },
+    'User': {
+        'dataSource': 'vagrant-postgres',
+        'public': false
+    },
+    'AccessToken': {
+        'dataSource': 'vagrant-postgres',
+        'public': false
+    },
+    'ACL': {
+        'dataSource': 'vagrant-postgres',
+        'public': false
+    },
+    'RoleMapping': {
+        'dataSource': 'vagrant-postgres',
+        'public': false
+    },
+    'Role': {
+        'dataSource': 'vagrant-postgres',
+        'public': false
+    },
+    'CommunityItem': {
+        'dataSource': 'memory-db', // This is an abstract model
+        'public': false,
+    },
+    'Review': {
+        'dataSource': 'vagrant-postgres',
+        'public': true,
+        '$promise': {},
+        '$resolved': true
+    },
+    'Comparison': {
+        'dataSource': 'vagrant-postgres',
+        'public': true,
+        '$promise': {},
+        '$resolved': true
+    },
+    'Howto': {
+        'dataSource': 'vagrant-postgres',
+        'public': true,
+        '$promise': {},
+        '$resolved': true
+    },
+    'Question': {
+        'dataSource': 'vagrant-postgres',
+        'public': true,
+        '$promise': {},
+        '$resolved': true
+    },
+    'Topic': {
+        'dataSource': 'vagrant-postgres',
+        'public': true
+    },
+    'Vote': {
+        'dataSource': 'vagrant-postgres',
+        'public': true
+    }
 }
