3e2d4b784372a866bb90c00cc95e936b7633accc
James Koshigoe <james@jameskoshigoe.com>
2016-10-07 17:23:29 -0600

    Attempt at duplicate topic disambiguator
    I tried adding a disambiguator field to the topics model, but was once again
    stopped by bugs in Loopback. Loopback was not letting the disambiguator
    column to be non-null with a default value of ''. This is the only way to
    enforce a unique constraint on topic name+disambiguator since PostgreSQL does
    not consider duplicate null values as unique. The only solution seems to take
    Loopback to a field and beat the shit out of it with a baseball bat while
    gangsta rap plays in the background.

--- pz-server/src/models/topic.ts
+++ pz-server/src/models/topic.ts
@@ -1,9 +1,3 @@
-import {
-    IRepository,
-    IRepositoryRecord,
-    createRecordFromLoopback
-} from 'pz-server/src/support/repository';
-
 import {ISluggable, ISluggableInstance} from 'pz-server/src/url-slugs/mixins/sluggable';
 import {ICommunityItemInstance} from 'pz-server/src/models/community-item';
 import {IPhotoInstance} from 'pz-server/src/models/photo';
@@ -21,10 +15,15 @@ export interface ITopicModel extends IPersistedModel, ISluggable {
 export interface ITopicInstance extends IPersistedModelInstance, ISluggableInstance {
     id: number
     name: string
-    description: string
-    overviewContent: string
+
+    // TODO: name and disambiguator were going to be a unique key, but it wasn't working
+    // TODO: Loopback doesn't allow nullable columns so the unique constraint failed
+    // disambiguator: string | null
+
+    description: string | null
+    overviewContent: string | null
     isVerified: boolean
-    thumbnailPhotoPath: string
+    thumbnailPhotoPath: string | null
     communityItems: IRelatedPersistedModel<ICommunityItemInstance>
     photos: IRelatedPersistedModel<IPhotoInstance>
     reviews: IRelatedPersistedModel<ICommunityItemInstance>
