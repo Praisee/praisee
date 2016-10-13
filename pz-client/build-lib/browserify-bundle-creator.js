111d6d3fd3d604117853195ceb6da2e47f89097c
James Koshigoe <james@jameskoshigoe.com>
2016-10-13 04:24:12 -0600

    Build system tweaks for debugging

--- pz-client/build-lib/browserify-bundle-creator.js
+++ pz-client/build-lib/browserify-bundle-creator.js
@@ -1,11 +1,11 @@
 var browserify = require('browserify');
 var pzPath = require('pz-support/pz-path');
 
-module.exports = function createBrowserifyBundle() {
-    var browserified = browserify({
+module.exports = function createBrowserifyBundle(browserifyOptions) {
+    var browserified = browserify(Object.assign({}, browserifyOptions, {
         entries: pzPath('pz-client', 'build/src/index.js'),
         debug: true
-    });
+    }));
 
     return browserified.bundle();
 };
--- pz-client/build-lib/dev-build-sources-task.js
+++ pz-client/build-lib/dev-build-sources-task.js
@@ -10,12 +10,16 @@ module.exports = function(gulp) {
     var dependencies = [transpile(gulp)];
 
     gulp.task('pzClient:dev:buildSources', dependencies, function() {
-        return (browserifyBundleCreator()
+        var browserifyOptions = {
+            fullPaths: true
+        };
+
+        return (browserifyBundleCreator(browserifyOptions)
             .on('error', function (error) {
                 console.error(error.toString());
                 this.emit("end");
             })
-            
+
             .pipe(source('index.js'))
             .pipe(buffer())
 
