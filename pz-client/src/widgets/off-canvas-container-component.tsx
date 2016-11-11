b11a7a94117778960fd1789be14c75e4bcd3ec48
Luke Pammant <luke.pammant@gmail.com>
2016-11-11 00:20:16 -0700

    Added logic to toggle side bar

--- pz-client/src/widgets/off-canvas-container-component.tsx
+++ pz-client/src/widgets/off-canvas-container-component.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
-import {Component} from 'react';
+import { Component } from 'react';
+import classNames from 'classnames';
 
 export default class OffCanvasContainer extends Component<any, any> {
+    state = { active: false };
+
     render() {
+        var containerClasses = classNames("off-canvas-container", { "off-canvas-container-active": this.state.active });
+
         return (
             <div className="off-canvas-wrapper" >
-                <div className="off-canvas-container" >
+                <button onClick={this._toggleOffCanvasContainer.bind(this)}>toggle side</button>
+                
+                <div className={containerClasses} >
                     {this.props.children}
                 </div>
             </div>
         )
+    }
+
+    private _toggleOffCanvasContainer() {
+        this.setState({ active: !this.state.active });
     }
 }
 
