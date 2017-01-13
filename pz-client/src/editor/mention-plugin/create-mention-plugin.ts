6629861d6fdaa72be5f48470c77455a5ae99545c
James Koshigoe <james@jameskoshigoe.com>
2017-01-12 17:27:50 -0800

    Turned off editor mention plugin due to its bugs

--- pz-client/src/editor/mention-plugin/create-mention-plugin.ts
+++ pz-client/src/editor/mention-plugin/create-mention-plugin.ts
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import ContentBlock = Draft.Model.ImmutableData.ContentBlock;
 import createMentionSuggestions from 'pz-client/src/editor/mention-plugin/mention-suggestions';
 import {ClickableMentionLink, EditableMentionLink} from 'pz-client/src/editor/mention-plugin/mention-link-component';
@@ -5,30 +6,49 @@ import {ClickableMentionLink, EditableMentionLink} from 'pz-client/src/editor/me
 var createMentionsPlugin = require('draft-js-mention-plugin').default as Function;
 
 export default function createMentionPlugin({isEditable}: {isEditable: boolean}) {
-    const mentionsPlugin = createMentionsPlugin({
-        entityMutability: 'IMMUTABLE',
-        mentionComponent: isEditable ? EditableMentionLink : ClickableMentionLink
-    });
+    // TODO: Disabled Mention plugin because it is very sick :(
+    // This plugin seems to have a lot of issues.
+    //
+    // First, we tried to allow triggering mentions when the user typed in an
+    // uppercase letter, but that broke it.
+    //
+    // Then it broken more recently when a user tried to @ mention a user which was
+    // not a supported feature yet. So it created a mention entity with no data
+    // and subsequently would fail the entire render.
+    //
+    // We need to revisit this plugin when it is more stable
+    // Last tested with v2 beta 1
+    //
+    //
 
-    const {MentionSuggestions: PluginMentionSuggestions} = mentionsPlugin;
+    return {
+        MentionSuggestions: () => React.createElement('span')
+    };
 
-    const [mentionsDecorator, mentionSuggestionsDecorator] = mentionsPlugin.decorators;
-
-    // const taggerTrigger = {
-    //     strategy: taggerTriggerStrategy,
-    //     component: mentionSuggestionsDecorator.component
-    // };
-
-    return Object.assign({}, mentionsPlugin, {
-        // MentionSuggestions: fixMentionSuggestions(MentionSuggestions),
-        MentionSuggestions: createMentionSuggestions(PluginMentionSuggestions),
-
-        decorators: [
-            mentionsDecorator,
-            // taggerTrigger
-            mentionSuggestionsDecorator
-        ]
-    });
+    // const mentionsPlugin = createMentionsPlugin({
+    //     entityMutability: 'IMMUTABLE',
+    //     mentionComponent: isEditable ? EditableMentionLink : ClickableMentionLink
+    // });
+    //
+    // const {MentionSuggestions: PluginMentionSuggestions} = mentionsPlugin;
+    //
+    // const [mentionsDecorator, mentionSuggestionsDecorator] = mentionsPlugin.decorators;
+    //
+    // // const taggerTrigger = {
+    // //     strategy: taggerTriggerStrategy,
+    // //     component: mentionSuggestionsDecorator.component
+    // // };
+    //
+    // return Object.assign({}, mentionsPlugin, {
+    //     // MentionSuggestions: fixMentionSuggestions(MentionSuggestions),
+    //     MentionSuggestions: createMentionSuggestions(PluginMentionSuggestions),
+    //
+    //     decorators: [
+    //         mentionsDecorator,
+    //         // taggerTrigger
+    //         mentionSuggestionsDecorator
+    //     ]
+    // });
 }
 
 // const taggerTriggers = /(\s|^)([@#]\w+|[A-Z]\w{1,})/g;
